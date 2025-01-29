c07962b7c51d90366d93d6d36457f0e39d737814
Anton Titov <puck.xi@gmail.com>
2025-01-29 13:23:37 +0800

    Hello

---
+++ src/main/java/test/test.java
@@ -0,0 +1,21 @@
+package test;
+
+public class test {
+    public int a = 10;
+
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    @Override
+    public String toString() {
+        return "test{" +
+                "a=" + a +
+                '}';
+    }
+}
